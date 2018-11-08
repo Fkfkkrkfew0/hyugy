9743b282ad2ac4d80c2f215b320884196f444618
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-11-08 17:41:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -799,19 +799,6 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTimestamp()
-            .setTitle('رساله جديده في خاص البوت')
-            .setThumbnail(${message.author.avatarURL})
-            .setDescription(\n\n\``${message.content}```)
-            .setFooter(من (@${message.author.tag})  |  (${message.author.id})`)
-        client.channels.get("506778257308385290").send({ embed: stewart });
-    }
-});
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
